82699faf15b9445bbbe898f802ae3d03060ac072
Minh Tu Ngo <tungominh.hust@gmail.com>
2023-04-04 11:27:44 +0700

    Add repositories

--- src/main/java/com/nmintu/spring_jwt/repository/RoleRepository.java
+++ src/main/java/com/nmintu/spring_jwt/repository/RoleRepository.java
@@ -1,2 +1,14 @@
-package com.nmintu.spring_jwt.repository;public interface RoleRepository {
+package com.nmintu.spring_jwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nmintu.spring_jwt.models.ERole;
+import com.nmintu.spring_jwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    Optional<Role> findByName(ERole name);
 }
--- src/main/java/com/nmintu/spring_jwt/repository/UserRepository.java
+++ src/main/java/com/nmintu/spring_jwt/repository/UserRepository.java
@@ -1,2 +1,18 @@
-package com.nmintu.spring_jwt.repository;public interface UserRepository {
+package com.nmintu.spring_jwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nmintu.spring_jwt.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email)''
 }
+
